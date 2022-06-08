8fd45922e7361a642c74855109572996c73c9ca4
phunzion <phunzion@gmail.com>
2022-06-08 13:23:23 +0200

    start

--- index.js
+++ index.js
@@ -10,9 +10,15 @@ client.on('ready', () => {
 client.on('interactionCreate', async interaction => {
   if (!interaction.isCommand()) return;
 
-  if (interaction.commandName === 'ping') {
-    await interaction.reply('Pong!');
-  }
-});
+  const { commandName } = interaction;
+
+  if (commandName === 'ping') {
+		await interaction.reply('Pong!');
+	} else if (commandName === 'server') {
+		await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
+	} else if (commandName === 'user') {
+		await interaction.reply(`Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`);
+	}
+})
 
 client.login(token);
