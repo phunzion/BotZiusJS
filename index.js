804d8ab69fd4893e5b5f5a753b48dbc52321b3e8
phunzion <phunzion@gmail.com>
2022-06-20 15:28:20 +0200

    start

--- index.js
+++ index.js
@@ -17,28 +17,6 @@ for (const file of commandFiles) {
 
 client.once('ready', () => {
 	console.log('Ready!');
-
-	const exampleEmbed = new MessageEmbed()
-		.setColor('#0099ff')
-		.setTitle('Some title')
-		.setURL('https://discord.js.org/')
-		.setAuthor({ name: 'Some name', iconURL: 'https://i.imgur.com/AfFp7pu.png', url: 'https://discord.js.org' })
-		.setDescription('Some description here')
-		.setThumbnail('https://i.imgur.com/AfFp7pu.png')
-		.addFields(
-			{ name: 'Regular field title', value: 'Some value here' },
-			{ name: '\u200B', value: '\u200B' },
-			{ name: 'Inline field title', value: 'Some value here', inline: true },
-			{ name: 'Inline field title', value: 'Some value here', inline: true },
-		)
-		.addField('Inline field title', 'Some value here', true)
-		.setImage('https://i.imgur.com/AfFp7pu.png')
-		.setFooter({ text: 'Some footer text here', iconURL: 'https://i.imgur.com/AfFp7pu.png' })
-		.setTimestamp();
-
-	const channel = client.channels.resolve('767020790117433354');
-	// console.log(channel);
-	channel.send({ embeds: [exampleEmbed] });
 });
 
 client.on('interactionCreate', async interaction => {
